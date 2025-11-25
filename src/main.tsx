c7ddce1d16c196b675917861a3dc51503ae614e6
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-11-25 21:34:44 +0000

    feat: bootstrap included and hardcopy css files removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
