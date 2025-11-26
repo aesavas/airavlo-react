032179a95deab3dc4a6401323140183a2c2f7fe3
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-11-26 10:47:02 +0000

    feat: DataFilters component created to merge all filter components

---
+++ src/components/DataFilters.tsx
@@ -0,0 +1,65 @@
+import ContinentFilter from '@/components/filters/ContinentFilter'
+import CountryFilter from '@/components/filters/CountryFilter'
+import PassengerVolumeFilter from '@/components/filters/PassengerVolumeFilter'
+import TypeFilter from '@/components/filters/TypeFilter'
+import type { IFilters } from '@/types'
+import { useState } from 'react'
+
+function DataFilters({filters, setFilters}: {filters: IFilters, setFilters: (value: IFilters) => void}) {
+  const [showMoreFilters, setShowMoreFilters] = useState(false)
+
+  const clearFilters = () => {
+    setFilters({
+      continent: "All",
+      country: "All",
+      passengerVolume: "All",
+      type: "All"
+    })
+  }
+
+  return (
+      <>
+        <div className="row align-items-stretch flex-column-reverse flex-md-row mb-3 mb-md-0">
+          <div className="col-md-8">
+            <div className="row">
+              <ContinentFilter className="col-12 col-md-6 mb-3 mb-md-0" filters={filters} setFilters={setFilters} />
+              <CountryFilter className="col-12 col-md-6" filters={filters} setFilters={setFilters} />
+            </div>
+          </div>
+          <div className="col-md-4 d-flex justify-content-md-end mb-3 mb-md-0">
+            <button
+              className="btn btn-secondary"
+              style={{height: "3.5rem"}}
+              onClick={() => setShowMoreFilters(!showMoreFilters)}
+            >
+              {showMoreFilters ? 'Less Filters' : 'More Filters'}
+            </button>
+            <button
+              className="btn btn-secondary ms-2"
+              style={{height: "3.5rem"}}
+              onClick={clearFilters}
+            >
+              Clear Filter
+            </button>
+          </div>
+        </div>
+
+        <div className={`collapse ${showMoreFilters ? 'show' : ''}`}>
+          <div className="row">
+            <PassengerVolumeFilter
+              className="col-12 col-md-4 mb-3 mb-md-0"
+              filters={filters}
+              setFilters={setFilters}
+            />
+            <TypeFilter
+              className="col-12 col-md-4"
+              filters={filters}
+              setFilters={setFilters}
+            />
+          </div>
+        </div>
+      </>
+  )
+}
+
+export default DataFilters
